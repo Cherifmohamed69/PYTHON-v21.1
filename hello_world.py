a2a1eec97852359bd78cc04b9310f29f512dabaa
Cherifmohamed69 <coachcherifmohamed@gmail.com>
2023-05-16 12:35:03 +0200

    hello world

--- hello_world.py
+++ hello_world.py
@@ -2,9 +2,9 @@ print( "Hello World" )
 name = "Cherif"
 print( "Hello", name,"!" )
 print( "Hello " + name + "!" )
-name = 22
-print("Hello" , 22, "!" )
-print( "Hello " + str(22) + "!")
+name = 8
+print("Hello" , 8, "!" )
+print( "Hello " + str(8) + "!")
 fave_food1 = "sushi"
 fave_food2 = "pizza"
 print( "I love to eat {} and {}.".format(fave_food1,fave_food2) )

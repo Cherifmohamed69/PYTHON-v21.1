bb006851e81e65a944b59fb3ce941ac6965ade47
Cherifmohamed69 <coachcherifmohamed@gmail.com>
2023-05-17 20:12:49 +0200

    comit

--- week 1/Day 1/Practice/Functions Basic II/FunctionsBasicII.py
+++ week 1/Day 1/Practice/Functions Basic II/FunctionsBasicII.py
@@ -1,9 +1,42 @@
+# function 1
 for x in range(5, 0, -1):
     print(x)
 
+# function 2
 
-# def print_and_return(numbers):
-#     print(numbers[0])
-#     return numbers[1]
-# result = print_and_return([1,2])
-# print(result)
+def print_and_return(numbers):
+    print(numbers[0])
+    return numbers[1]
+result = print_and_return([1,2])
+print(result)
+
+# function 3
+
+def first_plus_length(lst):
+    return lst[0] + len(lst)
+result = first_plus_length([1, 2, 3, 4, 5])
+print(result)
+
+# function 4
+
+def values_greater_than_second(lst):
+    if len(lst) < 2:
+        return False
+
+    second_value = lst[1]
+    new_list = [num for num in lst if num > second_value]
+
+    print(len(new_list))
+    return new_list
+print(values_greater_than_second([5, 2, 3, 2, 1, 4]))
+print(values_greater_than_second([3]))
+
+
+# function 5
+
+def length_and_value(size, value):
+    return [value] * size
+
+print(length_and_value(4, 7))
+
+print(length_and_value(6, 2))

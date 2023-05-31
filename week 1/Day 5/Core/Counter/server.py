80fe33c602aecd85a1645cdb30c4e8e5f11b399b
Cherifmohamed69 <coachcherifmohamed@gmail.com>
2023-05-31 13:07:03 +0200

    ccccccc

--- week 1/Day 5/Core/Counter/server.py
+++ week 1/Day 5/Core/Counter/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, session
 
 app = Flask(__name__)
-app.secret_key="jfiwjeofeiu9532402-kofwjwiofkpkf-39-0"
+app.secret_key="shhhhhhhhhhhhhhhhhh"
 
 @app.route('/')
 def index():
